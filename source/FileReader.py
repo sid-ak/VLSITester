32d60c6269c6048020663deafe027c50261bc4c3
Nickmiller1212 <nickmiller12121998@gmail.com>
2022-11-22 21:42:58 -0500

    BrainStorming

--- source/FileReader.py
+++ source/FileReader.py
@@ -1,23 +1,46 @@
-primaryInputs = []
-primaryOutputs = []
-layout = []
-tabLineCount = 0
+
+def ReadFile():
+    primaryInputs = {}
+    primaryOutputs = {}
+    layout = {}
+    tabLineCount = 0
+
+    file = open('netlist.txt', 'r')
+    read = file.readlines()
+    for line in read:
+        if line[0] != '$':
+            if line[0] == '\t':
+                tabLineCount += 1
+            if line[0] != '\t' and tabLineCount == 0:
+                primaryInputs[line.split(' ')[0]] = [False]
+            elif line[0] != '\t' and line[0] != '\n' and tabLineCount == 1:
+                primaryOutputs[line.split(' ')[0]] = [False]
+            elif line[0] == '\t' and tabLineCount > 2:
+                # layout[line.split('\t')[1].split('\n')[0]] = [False]
+                temp = line.split('\t')[1].split('\n')[0]
+                gateDetails = temp.split(' ')
+                while "" in gateDetails:
+                    gateDetails.remove("")
+                output = gateDetails[0]
+                gateType = gateDetails[1]
+                input1 = gateDetails[2]
+                input2 = gateDetails[3]
+                if input1 in primaryInputs:
+                    print("found")
+                    input1 = primaryInputs[input1]
+                    print(input1)
+                if input2 in primaryInputs:
+                    input2 = primaryInputs[input2]
 
 
-file = open('test.txt', 'r')
-read = file.readlines()
-for line in read:
-    if line[0] != '$':
-        if line[0] == '\t':
-            tabLineCount += 1
-        if line[0] != '\t' and tabLineCount == 0:
-            primaryInputs.append(line.split(' ')[0])
-        elif line[0] != '\t' and line[0] != '\n' and tabLineCount == 1:
-            primaryOutputs.append(line.split(' ')[0])
-        elif line[0] == '\t' and tabLineCount > 2:
-            layout.append(line.split('\t')[1].split('\n')[0])
-file.close()
-print(primaryInputs)
-print(primaryOutputs)
-print(layout)
+
+    file.close()
+    print(primaryInputs)
+    print(primaryOutputs)
+    print(layout)
+    return {
+        "PrimaryInputs" : primaryInputs,
+        "PrimaryOutputs" : primaryOutputs,
+        "Layout" : layout
+    }
         
---
+++ source/main.py
@@ -0,0 +1,26 @@
+import FileReader as readFile
+circuit = {}
+while(True):
+    print("[0] Read the input net-list")
+    print("[1] Preform fault collapsing")
+    print("[2] List fault classes")
+    print("[3] Simulate")
+    print("[4] Generate tests (D-Algorithm)")
+    print("[5] Exit")
+
+    choice = input()
+    if choice == "0":
+        circuit = readFile.ReadFile()
+        print(choice)
+    elif choice == "1":
+        print(choice)
+    elif choice == "2":
+        print(choice)
+    elif choice == "3":
+        print(choice)
+    elif choice == "4":
+        print(choice)
+    elif choice =="5":
+        print(choice)
+        break
+
