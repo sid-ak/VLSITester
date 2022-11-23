5d1acf792e79d985eaa74385946dcd51b6f82156
sid-ak <sidharthak25@gmail.com>
2022-11-23 12:09:26 -0500

    1,2: Cleanup in main.py and FileReader.py

--- source/FileReader.py
+++ source/FileReader.py
@@ -1,43 +1,49 @@
 
 def ReadFile() -> dict:
-    primaryInputs = {}
-    primaryOutputs = {}
-    gate = []
-    gates = []
-    tabLineCount = 0
+    primaryInputs: dict = {}
+    primaryOutputs: dict = {}
+    gate: list = []
+    gates: list = []
+    tabLineCount: int = 0
 
-    file = open('netlist.txt', 'r')
-    read = file.readlines()
-    for line in read:
+    with open('netlist.txt', 'r') as file:
+        fileLines: list[str] = file.readlines()
+    
+    for line in fileLines:
+        
         if line[0] != '$':
+            
             if line[0] == '\t':
                 tabLineCount += 1
+            
             if line[0] != '\t' and tabLineCount == 0:
                 primaryInputs[line.split(' ')[0]] = False
+            
             elif line[0] != '\t' and line[0] != '\n' and tabLineCount == 1:
                 primaryOutputs[line.split(' ')[0]] = False
-                #gets gatedetails
+                
+            #gets gate details
             elif line[0] == '\t' and tabLineCount > 2:
                 temp = line.split('\t')[1].split('\n')[0]
                 gateDetails = temp.split(' ')
+                
                 while "" in gateDetails:
                     gateDetails.remove("")
+                
                 #splits the details up
                 output = gateDetails[0]
                 gateType = gateDetails[1]
                 input1 = gateDetails[2]
+                
                 #not gate only has 1 input
                 if gateType == "NOT":
                     input2 = "NONE"
                 else:
                     input2 = gateDetails[3]
+                
                 gate = [output, gateType, input1, input2]
                 gates.append(gate)
 
-    file.close()
-    # print(primaryInputs)
-    # print(primaryOutputs)
-    # print(gates)
     return {
         "PrimaryInputs" : primaryInputs,
         "PrimaryOutputs" : primaryOutputs,
--- source/main.py
+++ source/main.py
@@ -1,29 +1,43 @@
 import FileReader as readFile
 import Simulate as simulation
-circuit = {}
-gates = []
+
+circuit: dict = {}
+gates: list = []
+
 while(True):
     print("[0] Read the input net-list")
-    print("[1] Preform fault collapsing")
+    print("[1] Perform fault collapsing")
     print("[2] List fault classes")
     print("[3] Simulate")
     print("[4] Generate tests (D-Algorithm)")
     print("[5] Exit")
 
-    choice = input()
-    if choice == "0":
-        circuit = readFile.ReadFile()
-    elif choice == "1":
-        print(choice)
-    elif choice == "2":
-        print(choice)
-    elif choice == "3":
-        inputVector = input("Enter your input vector " + str(len(list(circuit.values())[0]))+ " Primary Inputs: ")
-        setOfFaults = input("Input the set of faults (EX: gate1 Sa0): ")
-        simulation.Simulate(circuit, inputVector, setOfFaults)
-    elif choice == "4":
-        print(choice)
-    elif choice == "5":
-        print(choice)
-        break
-    print(circuit)
+    try:
+        choice: int = int(input())
+        
+        if choice == 0:
+            circuit = readFile.ReadFile()
+        
+        elif choice == 1:
+            print("Under Construction")
+        
+        elif choice == 2:
+            print("Under Construction")
+
+        
+        elif choice == 3:
+            inputVector = input("Enter your input vector " + str(len(list(circuit.values())[0]))+ " Primary Inputs: ")
+            setOfFaults = input("Input the set of faults (EX: gate1 Sa0): ")
+            simulation.Simulate(circuit, inputVector, setOfFaults)
+        
+        elif choice == 4:
+            print("Under Construction")
+        
+        elif choice == 5:
+            print("Under Construction")
+            break
+        
+        print(circuit)
+    
+    except Exception as e:
+        print(f"Invalid input or something went wrong.\n{e}")
