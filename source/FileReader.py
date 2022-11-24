bba8d3937fc3389b2d1a3df34bd86760d9158812
sid-ak <sidharthak25@gmail.com>
2022-11-24 16:24:09 -0500

    1,2: Wrapper ReadFile() in a try-except.

--- source/FileReader.py
+++ source/FileReader.py
@@ -1,52 +1,56 @@
 
-def ReadFile() -> dict:
+def ReadFile(fileName: str = "netlist.txt") -> dict:
     primaryInputs: dict = {}
     primaryOutputs: dict = {}
     gate: list = []
     gates: list = []
     tabLineCount: int = 0
 
-    with open('netlist.txt', 'r') as file:
-        fileLines: list[str] = file.readlines()
-    
-    for line in fileLines:
-        
-        if line[0] != '$':
-            
-            if line[0] == '\t':
-                tabLineCount += 1
-            
-            if line[0] != '\t' and tabLineCount == 0:
-                primaryInputs[line.split(' ')[0]] = False
-            
-            elif line[0] != '\t' and line[0] != '\n' and tabLineCount == 1:
-                primaryOutputs[line.split(' ')[0]] = False
-                
-            #gets gate details
-            elif line[0] == '\t' and tabLineCount > 2:
-                temp = line.split('\t')[1].split('\n')[0]
-                gateDetails = temp.split(' ')
-                
-                while "" in gateDetails:
-                    gateDetails.remove("")
-                
-                #splits the details up
-                output = gateDetails[0]
-                gateType = gateDetails[1]
-                input1 = gateDetails[2]
-                
-                #not gate only has 1 input
-                if gateType == "NOT":
-                    input2 = "NONE"
-                else:
-                    input2 = gateDetails[3]
-                
-                gate = [output, gateType, input1, input2]
-                gates.append(gate)
+    try:
 
-    return {
-        "PrimaryInputs" : primaryInputs,
-        "PrimaryOutputs" : primaryOutputs,
-        "Gates" : gates
-    }
+        with open(fileName, 'r') as file:
+            fileLines: list[str] = file.readlines()
         
+        for line in fileLines:
+            
+            if line[0] != '$':
+                
+                if line[0] == '\t':
+                    tabLineCount += 1
+                
+                if line[0] != '\t' and tabLineCount == 0:
+                    primaryInputs[line.split(' ')[0]] = False
+                
+                elif line[0] != '\t' and line[0] != '\n' and tabLineCount == 1:
+                    primaryOutputs[line.split(' ')[0]] = False
+                    
+                #gets gate details
+                elif line[0] == '\t' and tabLineCount > 2:
+                    temp = line.split('\t')[1].split('\n')[0]
+                    gateDetails = temp.split(' ')
+                    
+                    while "" in gateDetails:
+                        gateDetails.remove("")
+                    
+                    #splits the details up
+                    output = gateDetails[0]
+                    gateType = gateDetails[1]
+                    input1 = gateDetails[2]
+                    
+                    #not gate only has 1 input
+                    if gateType == "NOT":
+                        input2 = "NONE"
+                    else:
+                        input2 = gateDetails[3]
+                    
+                    gate = [output, gateType, input1, input2]
+                    gates.append(gate)
+
+        return {
+            "PrimaryInputs" : primaryInputs,
+            "PrimaryOutputs" : primaryOutputs,
+            "Gates" : gates
+        }
+
+    except Exception as e:
+        raise Exception(f"Something went wrong while reading inputs.\n{e}")
