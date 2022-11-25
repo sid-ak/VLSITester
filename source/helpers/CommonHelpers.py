05cf001c6250fc615303b720f50b7479aa6c9355
sid-ak <sidharthak25@gmail.com>
2022-11-25 12:26:59 -0500

    1, 2: Some more input vector validation.

---
+++ source/helpers/CommonHelpers.py
@@ -0,0 +1,4 @@
+class CommonHelpers:
+
+    def IsNotZeroOrOne(value: int) -> bool:
+        return value != 0 and value != 1
--- source/helpers/VectorHelpers.py
+++ source/helpers/VectorHelpers.py
@@ -1,3 +1,5 @@
+from helpers.CommonHelpers import CommonHelpers
+
 class VectorHelpers:
 
     def GetInputVector(inputStr: str, primaryInputsCount: int) -> list[int]:
@@ -9,10 +11,13 @@ class VectorHelpers:
             if len(inputVectorStr) != primaryInputsCount: raise Exception()
 
             inputVector: list[int] = list(map(lambda e: int(e.strip()), inputVectorStr))
+            for inputInt in inputVector:
+                if CommonHelpers.IsNotZeroOrOne(inputInt): raise Exception("Input vector must only contain 0 or 1.")
+
             return inputVector
         
         except Exception as e:
             raise Exception("Invalid vector input.\n" +
             "Input vector must be separated by commas " +
             f"and must be equal to length of primary inputs ({primaryInputsCount})\n"
-            "Example: 0, 1, 1, 0, 1")
+            f"Example: 0, 1, 1, 0, 1\n{e}")
