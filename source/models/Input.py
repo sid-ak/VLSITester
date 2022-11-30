8415798c9a136168b5f32033fd7e04872d262eff
sid-ak <sidharthak25@gmail.com>
2022-11-29 23:31:47 -0500

    1,2: Added an IsFanout flag to inputs and outputs.

--- source/models/Input.py
+++ source/models/Input.py
@@ -1,9 +1,17 @@
 class Input:
     Wire: str
     IsPrimary: bool
+    IsFanout: bool = False
     Value: int = -1
 
-    def __init__(self, wire: str, isPrimary: bool, value: int = -1):
+    def __init__(
+        self,
+        wire: str,
+        isPrimary: bool,
+        isFanout: bool = False,
+        value: int = -1):
+        
         self.Wire = wire
         self.IsPrimary = isPrimary
+        self.IsFanout = isFanout
         self.Value = value
